3c7c7227307ce2e27a343c8711f64b9e7c1193b9
ilyutoev <ilyutoev@gmail.com>
2020-10-14 21:06:00 +0300

    Небольшой рефакторинг

--- tg_bot.py
+++ tg_bot.py
@@ -1,14 +1,22 @@
 import re
 
 
-with open('questions.txt', 'r', encoding='KOI8-R') as f:
-    a = f.read()
+def get_questions():
+    """Открываем файл с вопросами и возвращаем словарь вопрос: ответ"""
+    with open('questions.txt', 'r', encoding='KOI8-R') as f:
+        questions_str = f.read()
 
-questions = {}
-question = ''
-for line in a.split('\n\n'):
-    if 'Вопрос' in line:
-        question = re.sub(r'Вопрос .+:?\n', '', line).replace('\n', ' ').strip()
-    if 'Ответ' in line:
-        answer = line.replace('Ответ:', '').replace('\n', ' ').strip()
-        questions[question] = answer
+    questions = {}
+    question = ''
+    for line in questions_str.split('\n\n'):
+        if 'Вопрос' in line:
+            question = re.sub(r'Вопрос .+:?\n', '', line).replace('\n', ' ').strip()
+        if 'Ответ' in line:
+            answer = line.replace('Ответ:', '').replace('\n', ' ').strip()
+            questions[question] = answer
+
+    return questions
+
+
+if __name__ == '__main__':
+    questions = get_questions()
